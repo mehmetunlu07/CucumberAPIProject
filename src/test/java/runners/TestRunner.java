93ab7bfb88181bde4e9f9ab0af5018f2450777d8
mehmetunlu07 <maxbusiness24@gmail.com>
2025-02-12 18:56:28 -0600

    Commit from Intellij

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,26 +1,28 @@
 package runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features="src/test/java/features",
         glue="stepDefinitions",
-        dryRun=false,    //false real run
-        tags="@smoke",
+        dryRun=false,    //false: real run  true:compiling and tell missing steps
+        tags="@tag1",
         monochrome = true,
         //strict=true,
         plugin={
                 "pretty",
                 "html:target/cucumber-report.html",
                 "json:target/cucumber.json",
-                "rerun:target/failed.txt"
+                "rerun:target/failed.txt",
+             //   "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" // Extent Reports adapter
         }
 
 )
 
-public class TestRunner {
+public class TestRunner  {
 
 
 }
